93a3d319cfa6c6accb40f0f6cf8df1ac8d02ab9a
Daniel Ruijs <ruijs@kth.se>
2026-05-20 16:17:57 +0200

    feat: add minimumReleaseAge of 7 days to config

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 module.exports = {
-    platform: 'github',
-    endpoint: 'https://api.github.com',
-    onboardingConfig: {
-        extends: [
-            'config:recommended',
-            'group:all'
-        ],
-    },
-    repositories: ['danielruijs/go-chess'],
+  platform: "github",
+  endpoint: "https://api.github.com",
+  minimumReleaseAge: "7 days",
+  onboardingConfig: {
+    extends: ["config:recommended", "group:all"],
+  },
+  repositories: ["danielruijs/go-chess"],
 };
